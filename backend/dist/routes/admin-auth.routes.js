9e90b964d1c38080702ac9c9351055e9a74fe5fe
krsachin241 <krsachin241@gmail.com>
2025-12-30 09:59:20 +0530

    clear build

--- backend/dist/routes/admin-auth.routes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const admin_auth_controller_1 = require("../controllers/admin-auth.controller");
-const router = (0, express_1.Router)();
-router.post('/register', admin_auth_controller_1.adminRegister);
-router.post('/verify-otp', admin_auth_controller_1.adminVerifyOTPAndRegister);
-router.post('/login', admin_auth_controller_1.adminLogin);
-exports.default = router;
--- backend/dist/routes/platform-stats.routes.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const platform_stats_controller_1 = require("../controllers/platform-stats.controller");
-const router = (0, express_1.Router)();
-router.get('/stats', platform_stats_controller_1.getPlatformStats);
-exports.default = router;
